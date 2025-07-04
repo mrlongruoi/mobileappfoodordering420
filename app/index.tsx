973f346f39b7d4ac4fbcd42fe7687038aaaf3f06
mrlongruoi <mrlong01012020@gmail.com>
2025-07-04 22:19:46 +0700

    01: setup project expo & nativewind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+        Chào mừng tới ứng dụng Mobile!
+      </Text>
+    </View>
+  );
+}
